66dcb9b01efe88bfa50fc1f6f733364ca6b31f62
inv4fee2020 <inv4fee2020@gmail.com>
2023-08-28 15:13:55 +0100

    bases sys script error

--- pli_node_scripts.sh
+++ pli_node_scripts.sh
@@ -199,7 +199,7 @@ FUNC_NODE_DEPLOY(){
     FUNC_VARS;
 
     # call base_sys_setup script to perform basic system updates etc.
-    bash base_sys_setup.sh -D
+    source ~/pli2vn/base_sys_setup.sh -D
 
     echo
     echo -e "${GREEN}#########################################################################"
