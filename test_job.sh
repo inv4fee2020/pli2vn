7673f245d71c3ffb9b469e842c89125e7e6a3725
inv4fee2020 <inv4fee2020@gmail.com>
2023-09-14 23:00:42 +0100

    jobs

--- test_job.sh
+++ test_job.sh
@@ -36,21 +36,21 @@ minIncomingConfirmations = 0
 observationSource = """
     decode_log   [type="ethabidecodelog"
                   abi="OracleRequest(bytes32 indexed specId, address requester, bytes32 requestId, uint256 payment, address callbackAddr, bytes4 callbackFunctionId, uint256 cancelExpiration, uint256 dataVersion, bytes data)"
-                  data="$(jobRun.logData)"
-                  topics="$(jobRun.logTopics)"]
+                  data="/$(jobRun.logData)"
+                  topics="/$(jobRun.logTopics)"]
 
-    decode_cbor  [type="cborparse" data="$(decode_log.data)"]
+    decode_cbor  [type="cborparse" data="/$(decode_log.data)"]
     fetch        [type="http" method=GET url="https://min-api.cryptocompare.com/data/price?fsym=XDC&tsyms=USD" allowUnrestrictedNetworkAccess="true"]
-    parse        [type="jsonparse" path="USD" data="$(fetch)"]
+    parse        [type="jsonparse" path="USD" data="/$(fetch)"]
 
-    multiply     [type="multiply" input="$(parse)" times="$(decode_cbor.times)"]
+    multiply     [type="multiply" input="/$(parse)" times="/$(decode_cbor.times)"]
 
-    encode_data  [type="ethabiencode" abi="(bytes32 requestId, uint256 value)" data="{ \\"requestId\\": $(decode_log.requestId), \\"value\\": $(multiply) }"]
+    encode_data  [type="ethabiencode" abi="(bytes32 requestId, uint256 value)" data="{ /\/\"requestId/\/\": /$(decode_log.requestId), /\/\"value/\/\": /$(multiply) }"]
     encode_tx    [type="ethabiencode"
                   abi="fulfillOracleRequest2(bytes32 requestId, uint256 payment, address callbackAddress, bytes4 callbackFunctionId, uint256 expiration, bytes calldata data)"
-                  data="{\\"requestId\\": $(decode_log.requestId), \\"payment\\":   $(decode_log.payment), \\"callbackAddress\\": $(decode_log.callbackAddr), \\"callbackFunctionId\\": $(decode_log.callbackFunctionId), \\"expiration\\": $(decode_log.cancelExpiration), \\"data\\": $(encode_data)}"
+                  data="{/\/\"requestId/\/\": /$(decode_log.requestId), /\/\"payment/\/\":   $(decode_log.payment), /\/\"callbackAddress/\/\": /$(decode_log.callbackAddr), /\/\"callbackFunctionId/\/\": /$(decode_log.callbackFunctionId), /\/\"expiration/\/\": /$(decode_log.cancelExpiration), /\/\"data/\/\": /$(encode_data)}"
                   ]
-    submit_tx    [type="ethtx" to="$ORACLE_ADDR" data="$(encode_tx)"]
+    submit_tx    [type="ethtx" to="$ORACLE_ADDR" data="/$(encode_tx)"]
 
     decode_log -> decode_cbor -> fetch -> parse -> multiply -> encode_data -> encode_tx -> submit_tx
 """
