30c42eeec31286d53267c5879aa82ab26b355225
inv4fee2020 <inv4fee2020@gmail.com>
2023-10-22 23:15:32 +0100

    menu

--- dreq_job_menu.sh
+++ dreq_job_menu.sh
@@ -129,10 +129,9 @@ FUNC_API_MENU(){
     # FETCH_URL="https://min-api.cryptocompare.com/data/price?fsym=$_FYSM_INPUT&tsyms=$_TYSMS_INPUT"
 
     
-    declare -A _apiurl
-    _apiurl=( [Cryptocompare]="https://min-api.cryptocompare.com/data/price?fsym=$_FYSM_INPUT&tsyms=$_TYSMS_INPUT" \ 
-    [KuCoin]="https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=$_FYSM_INPUT-$_TYSMS_INPUT" \ 
-    [BiTrue]="https://openapi.bitrue.com/api/v1/ticker/price?symbol=$_FYSM_INPUT$_TYSMS_INPUT" )
+    declare -A _apiurl=( ['Cryptocompare']="https://min-api.cryptocompare.com/data/price?fsym=$_FYSM_INPUT&tsyms=$_TYSMS_INPUT" \ 
+    ['KuCoin']="https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=$_FYSM_INPUT-$_TYSMS_INPUT" \ 
+    ['BiTrue']="https://openapi.bitrue.com/api/v1/ticker/price?symbol=$_FYSM_INPUT$_TYSMS_INPUT" )
 
 
     #node_backup_arr=()
