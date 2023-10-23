4751fbcaaef89f1204c198512fd4a057396dc969
inv4fee2020 <inv4fee2020@gmail.com>
2023-10-23 10:37:20 +0100

    menu

--- dreq_job_menu.sh
+++ dreq_job_menu.sh
@@ -16,8 +16,8 @@ JOB_FNAME="pli2vn_testjob_CC_USD_XDC.toml"
 FUNC_GET_INPUTS(){
 
     echo -e "${GREEN}#"
-    echo -e "#   This script generates the necessary toml blob for the Job-Setup section in the docs"
-    echo -e "#   source: https://docs.goplugin.co/oracle/job-setup"
+    echo -e "#   This script generates the necessary toml blob for a Direct Request Job-Setup section in the docs"
+    echo -e "#   source: https://docs.goplugin.co/plugin-2.0/job-setup/steps-to-setup-direct-request-job"
     echo -e "#"
     echo -e "#   The script removes the "-" hyphen from the original returned 'external_job_id' value"
     echo -e "#"
@@ -28,22 +28,26 @@ FUNC_GET_INPUTS(){
     sleep 0.5s
     #source ~/"plinode_$(hostname -f)".vars
 
+
+    # initialise variables with no values
     _FSYM_INPUT=""
     _TSYMS_INPUT=""
 
     read -p 'Enter FROM Pair (fsym) ticker : ' _FSYM_INPUT
     read -p 'Enter TO Pair (tsyms) ticker : ' _TSYMS_INPUT
     echo "-----------------------------------------------"
-    #read -p 'Enter your Oracle Contract Address : ' _INPUT
-    ORACLE_ADDR="xdc268035b8D5d0a583007f6b8443E31C9c650FcbE1"
-    #ORACLE_ADDR="$(echo $_INPUT | sed '/^$/d;/^\\\s*$/d;s/^xdc/0x/g')"
-    #FETCH_URL="https://min-api.cryptocompare.com/data/price?fsym=$_FYSM_INPUT&tsyms=$_TYSMS_INPUT"
+    echo
+    read -p 'Enter your Oracle Contract Address : ' _INPUT
+    echo "-----------------------------------------------"
+    #ORACLE_ADDR="xdc268035b8D5d0a583007f6b8443E31C9c650FcbE1"
+    ORACLE_ADDR="$(echo $_INPUT | sed '/^$/d;/^\\\s*$/d;s/^xdc/0x/g')"
     FUNC_API_MENU;
 }
 
 
 FUNC_CREATE_JOB(){
 
+# Creates the job file and passed variable values 
 cat <<EOF > ~/$JOB_FNAME
 type = "directrequest"
 schemaVersion = 1
@@ -74,30 +78,41 @@ observationSource = """
 """
 EOF
 
+    # Checks that the node API is ready and accepting credentials
     plugin admin login -f $PLI_DEPLOY_PATH/apicredentials.txt > /dev/null 2>&1
     if [ $? != 0 ]; then
       echo
       echo  -e "${RED}## ERROR :: Plugin admin login encoutered issues${NC}"
-      sleep 2s
+      #sleep 2s
       exit
     else
       echo -e "${GREEN}INFO :: Successfully logged in with API credentials${NC}"
-      sleep 0.5s
+      #sleep 0.5s
     fi
     echo
+
+    # Outputs return response from node API to raw tmp file based on toml blob
     plugin jobs create ~/$JOB_FNAME > /tmp/plivn_job_id.raw
     if [ $? != 0 ]; then
       echo
       echo  -e "${RED}## ERROR :: Plugin JOBS creation encoutered issues${NC}"
+      cat /tmp/plivn_job_id.raw 
       sleep 2s
       exit
     else
       echo -e "${GREEN}INFO :: Successfully created JOB ID $JOB_TITLE ${NC}"
-      sleep 0.5s
+      cat /tmp/plivn_job_id.raw 
+      sleep 2s
     fi
     #echo
+
+    # Get Job ID for newly created job
     ext_job_id_raw="$(sudo -u postgres -i psql -d plugin_mainnet_db -t -c "SELECT external_job_id FROM jobs WHERE name = '$JOB_TITLE';")"
+    
+    # Remove hyphen separators
     ext_job_id=$(echo $ext_job_id_raw | tr -d \-)
+
+
     echo -e "${GREEN}---------------------------------------------------------------"
     echo
     echo -e "       Local node job id - Copy to your Solidity script"
@@ -106,7 +121,7 @@ EOF
     echo -e "Oracle Contract Address is :   $ORACLE_ADDR"
     echo -e "Job $JOB_TITLE ID is :   $ext_job_id "
     echo 
-    echo -e "URL for APIConsumer is :   $FETCH_URL${NC}"
+    echo -e "URL for APIConsumer is :\n   $FETCH_URL${NC}"
   }
 
 
@@ -125,77 +140,38 @@ FUNC_EXIT_ERROR(){
 
 FUNC_API_MENU(){
 
-    ## https://linuxhint.com/associative_array_bash/
-
-    ### Call the setup script to set permissions & check installed pkgs
-    #bash _plinode_setup_bkup.sh > /dev/null 2>&1
-
-    # FETCH_URL="https://min-api.cryptocompare.com/data/price?fsym=$_FYSM_INPUT&tsyms=$_TYSMS_INPUT"
-
-
-
-    #read -p 'Enter FROM Pair (fsym) ticker : ' _FSYM_INPUT && read -p 'Enter TO Pair (tsyms) ticker : ' _TSYMS_INPUT 
-    
+    # initialise the array with key:value pairs
     declare -A _apiurl=( 
     ["Cryptocompare"]="https://min-api.cryptocompare.com/data/price?fsym=$_FSYM_INPUT&tsyms=$_TSYMS_INPUT"
     ["KuCoin"]="https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=$_FSYM_INPUT-$_TSYMS_INPUT"
     ["BiTrue"]="https://openapi.bitrue.com/api/v1/ticker/price?symbol=$_FSYM_INPUT$_TSYMS_INPUT"
     )
-    echo
-    echo
-    for i in "${!_apiurl[@]}"; do
-      echo "API Provider: $i with URL ${_apiurl[$i]}"
-      echo "---------------------------------------"
-    done
-
-    #declare -a opt_api=()
-    #declare -A opt_url=()
-#
+    
     #for i in "${!_apiurl[@]}"; do
-    #  opt_api[$i]="${options[$i]%% *}"
-    #  opt_url[${opt_api[$i]}]="${_apiurl[$i]#* }"
+    #  echo "API Provider: $i with URL ${_apiurl[$i]}"
+    #  echo "---------------------------------------"
     #done
 
-
-    _apiurl_len=${#_apiurl[@]}
-    echo $_apiurl_len
+    #_apiurl_len=${#_apiurl[@]}
+    #echo $_apiurl_len
 
     echo
     echo "          Select the number for the API Provider you wish to use "
     echo
 
+    # Capture user input & call job creation function
     select _api in ${!_apiurl[@]} "QUIT" 
     do
         case "$_api" in
-            Cryptocompare) echo "   API Option: $_api" ; FETCH_URL=${_apiurl[$_api]}; FUNC_CREATE_JOB; break ;;
-            KuCoin) echo "   API Option: $_api" ; FETCH_URL=${_apiurl[$_api]}; FUNC_CREATE_JOB; break ;;
-            BiTrue) echo "   API Option: $_api" ; FETCH_URL=${_apiurl[$_api]}; FUNC_CREATE_JOB; break ;;
-            #${!_apiurl[@]}) echo "   API Option: $_api" ; FETCH_URL="${_apiurl[@]:1}"; FUNC_CREATE_JOB; break ;;
-            #${!_apiurl[@]}) echo "   API Option: $_api" ; FETCH_URL="${_apiurl[@]:2}"; FUNC_CREATE_JOB; break ;;
+            Cryptocompare) echo; echo "   API Option: $_api" ; FETCH_URL=${_apiurl[$_api]}; FUNC_CREATE_JOB; break ;;
+            KuCoin) echo; echo "   API Option: $_api" ; FETCH_URL=${_apiurl[$_api]}; FUNC_CREATE_JOB; break ;;
+            BiTrue) echo; echo "   API Option: $_api" ; FETCH_URL=${_apiurl[$_api]}; FUNC_CREATE_JOB; break ;;
             "QUIT") echo "exiting now..." ; FUNC_EXIT; break ;;
             *) echo invalid option;;
         esac
     done
 
 }
-
-####  #!/usr/bin/env bash
-####  
-####  declare -a opt_host=()   # Initialize our arrays, to make sure they're empty.
-####  declare -A opt_ip=()     # Note that associative arrays require Bash version 4.
-####  
-####  for i in "${!options[@]}"; do
-####    opt_host[$i]="${options[$i]%% *}"             # create an array of just names
-####    opt_ip[${opt_host[$i]}]="${options[$i]#* }"   # map names to IPs
-####  done
-####  
-####  PS3='Please enter your choice (q to quit): '
-####  select host in "${opt_host[@]}"; do
-####    case "$host" in
-####      "") break ;;  # This is a fake; any invalid entry makes $host=="", not just "q".
-####      *) ssh "${opt_ip[$host]}" ;;
-####    esac
-####  done
 
 
 FUNC_GET_INPUTS;
