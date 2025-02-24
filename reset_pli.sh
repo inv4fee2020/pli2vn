dced1b3a34d0c672cb8e9e20cc682ccd611958b5
inv4fee2020 <inv4fee2020@gmail.com>
2025-02-24 23:15:20 +0000

    test24

--- reset_pli.sh
+++ reset_pli.sh
@@ -99,7 +99,7 @@ fi
 
 
 # Remove all plugin, nodejs linked folders for current user & root
-cd ~/ && sudo sh -c "rm -rf .cache/ && rm -rf .nvm && rm -rf .npm && rm -rf .plugin && rm -rf Plugin && rm -rf .pm2 && rm -rf work && rm -rf go && rm -rf .yarn* && rm -f .local"
+cd ~/ ; sudo sh -c "rm -rf .cache/ && rm -rf .nvm && rm -rf .npm && rm -rf .plugin && rm -rf Plugin && rm -rf .pm2 && rm -rf work && rm -rf go && rm -rf .yarn* && rm -rf .local"
 
 
 if [ -e ~/.tmp_profile ]; then
